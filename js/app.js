a6cd90e6a522fa33ad6dbf56a4f96b0ff5dec5f5
mehmettunc55 <mtunc551980@gmail.com>
2021-04-24 14:24:40 +0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
 const counter = document.querySelector('#counter')
+const buttons = document.querySelectorAll('.counterBtn')
 
-
-
-const counter = document.querySelector('#counter');
-const buttons = document.querySelectorAll('.counterBtn');
 
 let count = 0;
-
-buttons.forEach(function(button){
+buttons.forEach(function(button){  
   button.addEventListener('click', function(){
-    if (button.classList.contains('prevBtn')){
+    if(button.classList.contains('prevBtn')){
       count--
     } else if (button.classList.contains('nextBtn')){
       count++
-    } 
+    }
 
-  counter.textContent = count;
+counter.textContent = count;
 
-  if (count < 0 ){
-        counter.style.color = 'red'
-      } else if (count > 0){
-        counter.style.color = 'green'
-      } else {
-        counter.style.color = 'black'
-      }
-  })
+  if(count<0){
+    counter.style.color = 'red';
+  } else if (count>0){
+    counter.style.color = '#7fb800';
+  }else {
+    counter.style.color = '#333333';
+  }
+})
 })
