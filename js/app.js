33b1bc76d59c2c7f2b3d8fe175e9b16deccd3c1b
mehmettunc55 <mtunc551980@gmail.com>
2021-04-24 04:38:26 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const counter = document.querySelector('#counter')
+
+
+
+const counter = document.querySelector('#counter');
+const buttons = document.querySelectorAll('.counterBtn');
+
+let count = 0;
+
+buttons.forEach(function(button){
+  button.addEventListener('click', function(){
+    if (button.classList.contains('prevBtn')){
+      count--
+    } else if (button.classList.contains('nextBtn')){
+      count++
+    } 
+
+  counter.textContent = count;
+
+  if (count < 0 ){
+        counter.style.color = 'red'
+      } else if (count > 0){
+        counter.style.color = 'green'
+      } else {
+        counter.style.color = 'black'
+      }
+  })
+})
